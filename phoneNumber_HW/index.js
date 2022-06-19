ac80b9d9356c0408c01cfc1f6172b79d0534cf50
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-06-19 13:50:02 +0800

    Update index.js

--- phoneNumber_HW/index.js
+++ phoneNumber_HW/index.js
@@ -7,12 +7,12 @@
 // */
 
 const createPhoneNumber = (numbers) => {
-    //return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6, 10).join('')}`;
-    let template = '(xxx) xxx-xxxx';
-    numbers.forEach(number => {
-        template = template.replace('x', number);
-    });
-    return template;
+  //return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6, 10).join('')}`;
+  let template = '(xxx) xxx-xxxx';
+  numbers.forEach((number) => {
+    template = template.replace('x', number);
+  });
+  return template;
 };
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
